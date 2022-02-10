033e120dfa922d597597649448978a420b92f821
palgy <palgiz@gmail.com>
2022-02-10 17:44:28 +0200

    Configurator3Base.cs - made this file empty for the current stage. All teh logic is in Configurator3.razor

--- BlazorServerApp1/Pages/Configurator3Base.cs
+++ BlazorServerApp1/Pages/Configurator3Base.cs
@@ -9,22 +9,6 @@ namespace BlazorServerApp1.Pages
 {
     public class Configurator3Base : ComponentBase
     {
-        public static List<string>? getCustomers()
-        {
-            try
-            {
-                string errMsg = string.Empty;
-                List<CUSTOMER_Class> values = PrApiCalls.getCustomers(ref errMsg);
-                List<string> lstCustDes = new List<string>();
-                lstCustDes.Add(" ");
-                lstCustDes.Add("aaaaaaaaaaaaaa");
-                lstCustDes.Add("bbb");
-                return lstCustDes;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
+        
     }
 }
