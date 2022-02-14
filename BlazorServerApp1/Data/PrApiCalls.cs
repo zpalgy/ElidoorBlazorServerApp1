6595eabaea00fb61ed2f7e35c0b29396ba4baf9d
palgy <palgiz@gmail.com>
2022-02-14 15:07:18 +0200

    Added tab control and tabPage (movingwing)

--- BlazorServerApp1/Data/PrApiCalls.cs
+++ BlazorServerApp1/Data/PrApiCalls.cs
@@ -58,6 +58,9 @@ namespace BlazorServerApp1.Data
 
         //public static RestClient restClient = new RestClient();  - this is page instance specific we can't make it application specific
         static string certAlert = "Pls check whether the SSL certificate of the Default Web Site on the web server has expired";
+
+        public static DoorConfig doorConfig = new DoorConfig();
+
         public static void initRestClient(RestClient restClient)
         {
             try
